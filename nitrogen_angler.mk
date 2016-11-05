928b06237dbd018c9b3c273c6ae1424c0d230a5d
zymphad <zymphad@gmail.com>
2016-11-05 08:26:00 -0400

    add nitrogen_angler.mk

---
+++ nitrogen_angler.mk
@@ -0,0 +1,42 @@
+#
+# Copyright 2016 Nitrogen Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit AOSP device configuration for angler.
+$(call inherit-product, device/huawei/angler/full_angler.mk)
+
+# Inherit some common Nitrogen stuff.
+$(call inherit-product, vendor/nitrogen/products/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aosip_angler
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := angler
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NBD90X/3254009:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NBD90X 3254009 release-keys" \
+    BUILD_ID=NRD90U \
+    BUILD_DISPLAY_ID=NBD90X
