1d1383c8d2b27ddf143f8057daa792b04823424f
zymphad <zymphad@gmail.com>
2016-11-05 08:46:00 -0400

    update vendor.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright 2013 The Android Open Source Project
+# Copyright 2016 Nitrogen Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +15,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_angler-userdebug
+add_lunch_combo nitrogen_angler-user
+add_lunch_combo nitrogen_angler-userdebug
+add_lunch_combo nitrogen_angler-end
